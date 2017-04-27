b2370b0b64d298fc97c1ab01dd70354aa2170338
Elgaoussi Hamza <rei-zer0@live.com>
2017-04-27 20:34:21 +0100

    Fix bad EOLs

--- localWeather.js
+++ localWeather.js
@@ -5,9 +5,9 @@
 $(document).ready(function() {
 	// Get the forcast and update the html
 	function getForecast(lat, lon) {
-		$.getJSON("http://api.openweathermap.org/data/2.5/weather?lat="
-					+ lat + "&lon=" + lon + "&APPID=27c54beef6c753f6da8ce9954b66293e"
-					, function(json) {
+		$.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" +
+			lat + "&lon=" + lon + "&APPID=27c54beef6c753f6da8ce9954b66293e",
+			function(json) {
 
 			// Info to html
 			// Name
@@ -21,29 +21,30 @@ $(document).ready(function() {
 			var tempFahrenheit = (tempCelsius * (9 / 5) + 32);
 			$("#temp").html(Math.round(tempCelsius));
 			$("#degree").html(
-				'<div class="btn-group" role="group" aria-label="...">\
-					<button  id="celsius" type="button" class="btn btn-info active">\
-						<i class="wi wi-celsius"></i>\
-					</button>\
-					<button id="fahrenheit" type="button" class="btn btn-info">\
-						<i class="wi wi-fahrenheit"></i>\
-					</button>\
-				</div>')
-				$("#celsius").click(function() {
-					$("#temp").html(Math.round(tempCelsius));
-					$("#fahrenheit").removeClass("active");
-					$("#celsius").addClass("active");
-				});
-				$("#fahrenheit").click(function() {
-					$("#temp").html(Math.round(tempFahrenheit));
-					$("#celsius").removeClass("active");
-					$("#fahrenheit").addClass("active");
-				});
+				'<div class="btn-group" role="group" aria-label="...">' +
+					'<button  id="celsius" type="button" class="btn btn-info active">' +
+						'<i class="wi wi-celsius"></i>' +
+					'</button>' +
+					'<button id="fahrenheit" type="button" class="btn btn-info">' +
+						'<i class="wi wi-fahrenheit"></i>' +
+					'</button>' +
+				'</div>'
+			);
+			$("#celsius").click(function() {
+				$("#temp").html(Math.round(tempCelsius));
+				$("#fahrenheit").removeClass("active");
+				$("#celsius").addClass("active");
+			});
+			$("#fahrenheit").click(function() {
+				$("#temp").html(Math.round(tempFahrenheit));
+				$("#celsius").removeClass("active");
+				$("#fahrenheit").addClass("active");
+			});
 
 				// Icon per Weather state
 				var weatherStateId = json.weather[0].id;
 				if (!Date.now) {
-					Date.now = function() { return new Date().getTime(); }
+					Date.now = function() { return new Date().getTime(); };
 				}
 				var currTime = Math.floor(Date.now() / 1000);
 				if (currTime >= json.sys.sunrise && currTime < json.sys.sunset) {
